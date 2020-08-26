37267854356a3bb6f49e84aab5d83d457bc70df0
Ashawe <harshsaglani@gmail.com>
2020-08-26 16:14:05 +0530

    :zap: Remove unnecessary await for file write.
    :art: Remove unnecessary comments & Improve structure of code.
    recycle: Refactor Code

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*
+
+```A Program to find meanings of words from Google's Dictionary and write necessary details in markdown format.```
+
+API USED: https://github.com/meetDeveloper/googleDictionaryAPI
+API ENDPOINT: https://api.dictionaryapi.dev/api/v2/entries/<lanuage-code>/<word>
+AUTHOR: AshAwe (https://github.com/ashawe)
+*/
+
 let fs = require("fs");
 const http = require('https');
 
@@ -9,7 +18,7 @@ function wordsFromFileToArray(fileName) {
     return data.split(',');
 }
 
-// takes a word,output filename as arg. O/p: void. Appends Meaning to filename
+// sync function that takes a word and returns json response from dictionary API.
 function findMeaningFromAPI(word) {
     return new Promise((resolve, reject) => {
         http.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word, (response) => {
@@ -30,69 +39,42 @@ function findMeaningFromAPI(word) {
     });
 }
 
-function writeMeaningToFile(inputStream,filename) {
-    return new Promise((resolve, reject) => {
-        let stream = fs.createWriteStream(filename, { flags: 'a' });
-        let toWrite = "";
-        let result = JSON.parse(inputStream);
-        // console.log(JSON.parse(data));
-        // let result = data;
-        // toWrite += "<details><summary>" + "\n";
-        let synonyms = "synonyms: ";
-        // console.log("***************************")
-        console.log(result[0].word);
-        //innerHTML += result[0].word + "</summary>";
-        toWrite += "- " + result[0].word + "\n";
-        //console.log(result[0].meanings);
-        //console.log(result[0].meanings.length);
-        result[0].meanings.forEach(meaning => {
-            // console.log("" + meaning.partOfSpeech + ": ");
-            toWrite += "    - " + meaning.partOfSpeech + "\n";
-            //innerHTML += "* " + meaning.partOfSpeech + "<br>";
-            meaning.definitions.forEach(definition => {
-                // console.log("    - " + definition.definition);
-                toWrite += "        - " + definition.definition + "\n";
-                //innerHTML += "    * " + definition.definition + "<br>";
-                // console.log("    ex: " + definition.example);
-                toWrite += "        - ex: " + definition.example + "\n";
-                //innerHTML += "    * ex: " + definition.example + "<br>";
-                if(definition.synonyms !== undefined)
-                {
-                    synonyms += " " + definition.synonyms[0];
-                    for (let i = 1; i < 5; i++) {
-                        const synonym = definition.synonyms[i];
-                        synonyms = synonyms + ", " + synonym;
-                    }
+// takes a json input `inputStream` from Dictionary API and `filename` as argument
+// then writes word, its part of speech, definition and synonyms to `filename` in markdown.
+function writeMeaningToFile(inputStream, filename) {
+    let stream = fs.createWriteStream(filename, { flags: 'a' });
+    let toWrite = "";
+    let result = JSON.parse(inputStream);
+    let synonyms = "synonyms: ";
+    console.log(result[0].word);
+    toWrite += "- " + result[0].word + "\n";
+    result[0].meanings.forEach(meaning => {
+        toWrite += "    - " + meaning.partOfSpeech + "\n";
+        meaning.definitions.forEach(definition => {
+            toWrite += "        - " + definition.definition + "\n";
+            toWrite += "        - ex: " + definition.example + "\n";
+            if (definition.synonyms !== undefined) {
+                synonyms += " " + definition.synonyms[0];
+                for (let i = 1; i < 5; i++) {
+                    const synonym = definition.synonyms[i];
+                    synonyms = synonyms + ", " + synonym;
                 }
-            });
+            }
         });
-        // console.log(synonyms);
-        toWrite += "        - " + synonyms + "\n";
-        // innerHTML += "    * " + synonyms + "<br>";
-        // let word = document.createElement("div");
-        // console.log(word);
-        // let body = document.getElementById("body");
-        // console.log(innerHTML);
-        // body.innerHTML += innerHTML;
-        // toWrite += "</details>";
-        console.log("ending stream");
-        stream.write(toWrite);
-        stream.end();
-        // holds response from server that is passed when Promise is resolved
-        // console.log(response_body);
-        console.log("resolving");
-        resolve();
     });
+    toWrite += "        - " + synonyms + "\n";
+    console.log("ending stream");
+    stream.write(toWrite);
+    stream.end();
 }
 
-
-async function writeMeaningToFileHandler(word, filename) {
+// a handler function to wait for the HTTP response and then call function which writes meaning to file
+async function waitAndWriteMeaningToFileHandler(word, filename) {
     try {
         let http_promise = findMeaningFromAPI(word);
         let inputStream = await http_promise;
         console.log("got stream");
-        let file_writer_promise = writeMeaningToFile(inputStream,filename);
-        let dummyVariable = await file_writer_promise;
+        writeMeaningToFile(inputStream, filename);
         console.log("wrote to file");
     }
     catch (error) {
@@ -109,13 +91,9 @@ function wordMeaningToMarkdown(inputFileName, outputFileName) {
     arrayOfWords.forEach(word => {
         (async function () {
             // wait to http request to finish
-            await writeMeaningToFileHandler(word,outputFileName);
-            
-            // below code will be executed after http request is finished
-            console.log(2);
+            await waitAndWriteMeaningToFileHandler(word, outputFileName);
         })();
     });
 }
 
-// findMeaningAndWrite("averse","./words.md");
 wordMeaningToMarkdown("./words1.txt", "./words.md");
